d09e52106e501b4163a862e450103ed7a2e7da75
lidongtaolk <lidongtaolk@sina.com>
2018-06-02 18:19:17 +0800

    改bug

--- app/Http/Controllers/testEmailController.php
+++ app/Http/Controllers/testEmailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\user;
 
 use DB;
 use Mail;
@@ -12,7 +13,10 @@ class testEmailController extends Controller
     public function send(Request $request){
 	 $name=$request->input("username");
 	 $email=$request->input("email");
-	 if($name==null){
+	 $users=App\user::where('name',$name)
+			 ->first();
+	 //$results = DB::table("users")->select('select * from users where name', ['id' => 1]);
+	 if($users==null){
 	     DB::table("users")->insert('insert into users (name,email) values (?, ?)',[$name,$email]);
 	 }
 	// Mail::send($request->input('email'))
