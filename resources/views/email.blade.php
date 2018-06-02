bd6976c392942f8410f9c143802ccefb92ceaa1a
Your Name <lidongtaolk@sina.com>
2018-06-02 22:06:18 +0800

    邮件推送页面

---
+++ resources/views/email.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>email</title>
+    <style type="text/css">
+        body{
+            margin:0 auto;
+            width:500px;
+        }
+        p{
+            font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>Thanks！</h1>
+    <p>这是条很成熟的邮件</p>
+</body>
+</html>
