5826a8cdd008f4e248a1ee1952e8312b44eb7450
Your Name <lidongtaolk@sina.com>
2018-06-02 17:07:28 +0800

    lowlow的前端测试

---
+++ resources/views/test.blade.php
@@ -0,0 +1,23 @@
+<html><<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <script type="text/javascript">
+        function Email(){
+            alert("多谢光临小站！");
+        }
+    </script>
+</head>
+<body>
+<form action="" method="post">
+你是谁吖:<input type="username" id="username" placeholder="input your name">
+你的邮箱地址嘞:<input type="email" name="email" placeholder="input your email address">
+<input type="Submit" value="让我给你发邮件吧" onclick="Email()">
+</form>
+</body>
+</html>
