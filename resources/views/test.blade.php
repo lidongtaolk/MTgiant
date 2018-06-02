e0e6463aefa268c3cd54a87c150d1bb9d83ff557
Your Name <lidongtaolk@sina.com>
2018-06-02 17:16:51 +0800

    测试页面更改

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,4 +1,4 @@
-<html><<!DOCTYPE html>
+<html><!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
@@ -12,11 +12,21 @@
             alert("多谢光临小站！");
         }
     </script>
+    <style type="text/css">
+    body{
+        margin:0 auto;
+        width:500px;
+    }
+    p{
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+    </style>
 </head>
 <body>
+<h1>欢迎吖，这里是大山的测试页</h1>
 <form action="" method="post">
-你是谁吖:<input type="username" id="username" placeholder="input your name">
-你的邮箱地址嘞:<input type="email" name="email" placeholder="input your email address">
+<p id="first">你是谁吖:</p><input type="username" id="username" placeholder="input your name">
+<p id="second">你的邮箱地址嘞:</p><input type="email" name="email" placeholder="input your email address">
 <input type="Submit" value="让我给你发邮件吧" onclick="Email()">
 </form>
 </body>
