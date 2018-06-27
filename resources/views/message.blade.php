1233048c9e087f0dfde14b8e375c1cf506fbf127
Your Name <lidongtaolk@sina.com>
2018-06-27 14:48:48 +0800

    echart组件2

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -19,7 +19,7 @@
         })();
     </script>
     <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.js"></script>
-    <script src="/js/echart.common.min.js"></script>
+    <script src="/js/echart.min.js"></script>
     <script>
     $('document').ready(function(){
         console.log($('article pre'));
@@ -57,30 +57,32 @@
         //});
         //指定图标的配置和数据
         window.onload=function(){
-            var option = {
-            title:{
-                text:'ECharts 数据统计'
+        // 基于准备好的dom，初始化echarts实例
+        console.log("yes");
+        console.log(document.getElementById('chartmain'));
+        var myChart = echarts.init(document.getElementById('chartmain'));
+        console.log(myChart);
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: 'ECharts 入门示例'
             },
-            tooltip:{},
-            legend:{
-                data:['用户来源']
+            tooltip: {},
+            legend: {
+                data:['销量']
             },
-            xAxis:{
-                data:["Android","IOS","PC","Ohter"]
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
             },
-            yAxis:{
-
-            },
-            series:[{
-                name:'访问量',
-                type:'line',
-                data:[500,200,360,100]
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
             }]
         };
-        //初始化echarts实例
-        var myChart = echarts.init(document.getElementById('chartmain'));
 
-        //使用制定的配置项和数据显示图表
+        // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
         }
     </script>
@@ -88,6 +90,7 @@
         html{
             font-size:37.5px;
             font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            -webkit-font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         }
         body{
             background-color: aquamarine;
