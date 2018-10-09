78752a3bb998e1812621f211c0579b497de2fd3d
lidongtaolk <lidongtaolk@sina.com>
2018-10-09 20:39:52 +0800

    user auth

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,18 @@ Route::get('/message',function(){
 Route::get('/messageController',"messageController@save");
 
 Route::get('/messageJSON',"messageController@show");
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
