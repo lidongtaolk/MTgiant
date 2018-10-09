c53ca0596bbfb440f3470b444703657e770a1f73
Your Name <lidongtaolk@sina.com>
2018-10-09 19:13:36 +0800

    初始化页面布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://github.com/lidongtaolk/" target="_blank">MTgaint</a> 编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:lidongtaolk@sina.com">联系我</a></p>
+    </div>
+</footer>
